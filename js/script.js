ebd1803523ae580c76f0addb8189449334f8ff61
RobinGrit22 <112506404+RobinGrit22@users.noreply.github.com>
2022-12-19 10:38:20 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,58 +1,65 @@
 const btn = document.querySelector('button')
-let empty = document.querySelector('div')
-const error = document.querySelector('#error-message')
+let empty = document.querySelector('main')
+
 
 btn.addEventListener('click', (event) => {
     event.preventDefault()
-    error.innerHTML = ''
-
     const languageInput = document.getElementById('userInput')
-    const languageSearch = languageInput.value
+    const languageSearch = languageInput.value.toLowerCase();
     const url = `https://restcountries.com/v3.1/lang/${languageSearch}`
 
 
     fetch(url)
     .then( response=>{
-        console.log(response);
-        if(response.status >= 200 && response.status < 300){
-            return response.json()
-        }
-        else {
-          error.innerText = 'det gick inte'
-        }
-
+        
+        if(response.status >= 200 && response.status < 300)
+            return response.json();
+            else throw 'Funka inte, vänligen sök på ett språk';
+    }).catch(function(error) {
+        alert(error)
     })
-
-     
+         
     .then((data) => {
         empty.innerHTML = ''
-        console.log(data)
-        data.forEach(element => {
+        const popArray = [];
+        let popContainer = [];
         
+        data.forEach(element => {
+
+            popContainer = document.createElement('div')
+            empty.append(popContainer)
 
             const oficcialName = document.createElement('p')
-            empty.append(oficcialName)
+            popContainer.append(oficcialName)
             oficcialName.innerHTML = 'Official name: ' + element.name.official
 
             const subregion = document.createElement('p')
-            empty.append(subregion)
+            popContainer.append(subregion)
             subregion.innerHTML = 'Subregion: ' + element.subregion
 
             const capital = document.createElement('p')
-            empty.append(capital)
+            popContainer.append(capital)
             capital.innerText = 'Capital city: ' + element.capital
 
             const population = document.createElement('p')
-            
-            empty.append(population)
+            popContainer.append(population)
             population.innerText = 'Population: ' + element.population
+            popArray.push(element.population)
+            population.setAttribute('id', 'population');
 
             const imgFlag = document.createElement('img')
-            empty.append(imgFlag)
+            popContainer.append(imgFlag)
             imgFlag.src = element.flags.png
+            
+         });
 
-        
-        });
-    })
+         const populationMatch = (Math.max(...popArray))
+         const popTextArray = document.querySelectorAll('#population');
 
+         for(let i = 0; i < popTextArray.length; i++){
+              if(popTextArray[i].innerText == `Population: ${populationMatch}`){
+                popTextArray[i].style.border = '2px solid green'
+              }
+         }  
+    });
 });
